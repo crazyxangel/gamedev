71688c1f1c8e50a16ecbdb68db9e565107915388
seanhuybrechts <seanhuybrechts@hotmail.com>
2018-12-21 12:58:15 +0100

    gamdev

---
+++ Game_Examen/Game_Examen/Animation.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Examen
+{
+    class Animation
+    {
+        private List<Frame> frames;
+        public Frame CurrentFrame { get; set; }
+        public int MovesaSecond { get; set; }
+        private int counter = 0;
+        private double x = 0;
+        public double offset { get; set; }
+
+
+        private int _totalWidth = 0;
+
+        public Animation()
+        {
+            frames = new List<Frame>();
+            MovesaSecond = 1;
+        }
+
+        public void AddFrame(Rectangle rectangle)
+        {
+            Frame newFrame = new Frame()
+            {
+                SourceRectangle = rectangle,
+                //Duration = duration
+            };
+
+            frames.Add(newFrame);
+            CurrentFrame = frames[0];
+            offset = CurrentFrame.SourceRectangle.Width;
+            foreach (Frame f in frames)
+                _totalWidth += f.SourceRectangle.Width;
+        }
+       
+        public void Update(GameTime gameTime)
+        {
+            double temp = CurrentFrame.SourceRectangle.Width * ((double)gameTime.ElapsedGameTime.Milliseconds / 1000);
+
+            x += temp;
+            if (x >= CurrentFrame.SourceRectangle.Width / MovesaSecond)
+            {
+                Console.WriteLine(x);
+                x = 0;
+                counter++;
+                if (counter >= frames.Count)
+                    counter = 0;
+                CurrentFrame = frames[counter];
+                offset += CurrentFrame.SourceRectangle.Width;
+            }
+            if (offset >= _totalWidth)
+                offset = 0;
+        }
+    }
+}
---
+++ Game_Examen/Game_Examen/Controls.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Examen
+{
+    public abstract class Controls
+    {
+        public bool left { get; set; }
+        public bool right { get; set; }
+        public bool up { get; set; }
+        public bool down { get; set; }
+        public abstract void Update();
+    }
+    public class ControlP1 : Controls
+    {
+        public override void Update()
+        {
+            KeyboardState stateKey = Keyboard.GetState();
+
+            left = stateKey.IsKeyDown(Keys.Q);
+            up = stateKey.IsKeyDown(Keys.Z);
+            down = stateKey.IsKeyDown(Keys.S);
+            right = stateKey.IsKeyDown(Keys.D);
+
+        }
+    }
+    public class ControlP2 : Controls
+    {
+        public override void Update()
+        {
+            KeyboardState stateKey = Keyboard.GetState();
+
+            left = stateKey.IsKeyDown(Keys.Left);
+            up = stateKey.IsKeyDown(Keys.Up);
+            down = stateKey.IsKeyDown(Keys.Down);
+            right = stateKey.IsKeyDown(Keys.Right);
+
+        }
+    }
+}
